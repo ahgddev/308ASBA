75ca4e07232db9fe2687c894a488930e7c362fd6
ahgddev <ahgddev@gmail.com>
2024-02-11 10:08:30 -0700

    404 image shows when foodReq can't find the food requested.

--- src/api.js
+++ src/api.js
@@ -3,14 +3,25 @@ async function getFoodIMG(food){
 //    let foodReq = await axios.get("https://foodish-api.com/api/images/biryani", [
 //     url: '/user', baseURL: 'https://some-domain.com/api'])
 let foodName = String(food);
-
-let foodReq = await axios({
-    url: foodName,
-    method: 'get',
-    baseURL: 'https://foodish-api.com/api/images/'
-  });
-   let foodimg = document.createElement("img")
-   foodimg.src = foodReq.data.image
-    document.body.append(foodimg)
+if (foodName.match(/\d+/g)){
+    throw new Error("Invalid food name.")
 }
-getFoodIMG(biryani)
+
+try {
+    let foodReq = await axios({
+        url: foodName,
+        method: 'get',
+        baseURL: 'https://foodish-api.com/api/images/'
+      });
+       let foodimg = document.createElement("img")
+       foodimg.src = foodReq.data.image
+       document.body.append(foodimg)
+} catch (error) {
+    console.log(error + " Food not found.")
+    let foodimg = document.createElement("img")
+       foodimg.src = "images/404img.png"
+       document.body.append(foodimg)
+}
+
+}
+getFoodIMG("biryani")
