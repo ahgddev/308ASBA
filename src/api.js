c2c2a6147ea4973313ca5280e3bfb0ddda3c477b
ahgddev <ahgddev@gmail.com>
2024-02-11 13:22:55 -0700

    Changed food API used.

--- src/api.js
+++ src/api.js
@@ -1,20 +1,20 @@
 //For getting stuff from the foodish-api
-async function getFoodIMG(food) {
-  let foodName = String(food);
-  if (foodName.match(/\d+/g)) {
-    throw new Error("Invalid food name.");
-  }
+async function getFoodIMG(barcode) {
+    let foodBarCode = String(barcode);
+//   if (foodName.match(/\d+/g)) {
+//     throw new Error("Invalid food name.");
+//   }
 
 
   //Try to get the requested food image.
   try {
     let foodReq = await axios({
-      url: "pizza",
-      method: "get",
-      baseURL: "https://foodish-api.com/api/images/",
+        url: foodBarCode,
+        method: "get",
+        baseURL: "https://world.openfoodfacts.org/api/v2/search/"
     });
     let foodimg = document.createElement("img");
-    foodimg.src = foodReq.data.image;
+    foodimg.src = foodReq.data.products.find(item => item._id === foodBarCode).image_nutrition_small_url;
     document.body.append(foodimg);
   } catch (error) {
     //Throw an error if the food can't be found.
@@ -26,15 +26,15 @@ async function getFoodIMG(food) {
 }
 
 async function getFoodNutrition(barcode){
-    //Try to get the food nutrion value from another api
+    //Try to get the food nutrion value from another
     let foodBarCode = String(barcode);
-    let recipe = await axios({
+    let foodData = await axios({
         url: foodBarCode,
         method: "get",
         baseURL: "https://world.openfoodfacts.org/api/v2/search/"
       });  
-      console.log(recipe.data.products)
+      console.log(foodData.data.products.find(item => item._id === foodBarCode))
 }
 
-getFoodIMG("pizza");
-getFoodNutrition("30176204220A03")
+getFoodIMG("3017620422003");
+getFoodNutrition("3017620422003")
