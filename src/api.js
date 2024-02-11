1fc22fc4925eee799d25e9bfa515d210cb09b69d
ahgddev <ahgddev@gmail.com>
2024-02-11 09:30:59 -0700

    User can now define a food in the getFoodIMG function.

--- src/api.js
+++ src/api.js
@@ -1,8 +1,16 @@
-//For getting stuff from the API
+//For getting stuff from the foodish-api
 async function getFoodIMG(food){
-   let foodReq = await axios.get("https://foodish-api.com/api/images/biryani")
+//    let foodReq = await axios.get("https://foodish-api.com/api/images/biryani", [
+//     url: '/user', baseURL: 'https://some-domain.com/api'])
+let foodName = String(food);
+
+let foodReq = await axios({
+    url: foodName,
+    method: 'get',
+    baseURL: 'https://foodish-api.com/api/images/'
+  });
    let foodimg = document.createElement("img")
    foodimg.src = foodReq.data.image
     document.body.append(foodimg)
 }
-getFoodIMG()
+getFoodIMG(biryani)
