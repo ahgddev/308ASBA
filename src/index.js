afb6f4463876e70b60d9bc865f226d27e0b39460
ahgddev <ahgddev@gmail.com>
2024-02-12 06:57:43 -0700

    Search clears correctly.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {createProducts, clearProducts, defaultProducts } from "/src/classes.js";
 let searchBar = document.getElementById("search_bar");
 let submitBtn = document.getElementById("submit")
+let resetBtn = document.getElementById("reset_button")
 // Manage user changing things on the page
 
 
@@ -11,6 +12,13 @@ defaultProducts()
 submitBtn.addEventListener("click", function(event){
     let searchBarData = searchBar.value
     searchForFood(String(searchBarData));
+});
+
+//Reset the Search Bar and the products area
+resetBtn.addEventListener("click", function(event){
+    searchBar.value = "";
+    clearProducts();
+    defaultProducts();
 });
 
 function searchForFood(barcode){
